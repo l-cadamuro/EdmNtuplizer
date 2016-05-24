04b362f759d0a96b33ab8f23e9f13d17db48eb58
Luca Cadamuro <lc.cadamuro@gmail.com>
2016-05-24 12:20:37 +0200

    ntuplzier cfg

--- EdmNtuplizer/test/ntuplizer_all.py
+++ EdmNtuplizer/test/ntuplizer_all.py
@@ -5,26 +5,42 @@ import os
 
 PyFilePath = os.environ['CMSSW_BASE']+"/src/EdmNtuplizer/EdmNtuplizer/"
 
-execfile(PyFilePath+"test/tools/EDM_list_emul_MinBias_266667_lastEmulTag.py")
+execfile(PyFilePath+"test/tools/EDM_list_pattern17Evts.py")
 
 process = cms.Process("TEST")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")    
 
-# process.source = cms.Source("PoolSource",
-#     fileNames = cms.untracked.vstring(
-#     'file:/home/llr/cms/cadamuro/MWGR2016_Analysis_Stage2/CMSSW_8_0_2/src/l1tCalo_2016_EDM.root',
-#     )
-# )
 process.source = cms.Source("PoolSource",
-    fileNames = FILELIST
+    fileNames = cms.untracked.vstring(
+    # 'file:/home/llr/cms/cadamuro/MWGR_802/CMSSW_8_0_2/src/RunOnPattern/dumpedFromData/l1tCalo_2016_simEDM_184Evts.root',
+    # 'file:/data_CMS/cms/cadamuro/test_submit_to_tier3/Stage2_Commissioning_MinBias_Run267593_reEmulSecTrimFix/l1tCalo_2016_simEDM_34.root',
+    # 'file:/data_CMS/cms/cadamuro/test_submit_to_tier3/Stage2_Commissioning_MinBias_Run267593_reEmulSecTrimFix/l1tCalo_2016_simEDM_35.root',
+    # 'file:/data_CMS/cms/cadamuro/test_submit_to_tier3/Stage2_Commissioning_MinBias_Run267593_reEmulSecTrimFix/l1tCalo_2016_simEDM_38.root'  
+    # 'file:/home/llr/cms/cadamuro/TauFixEmulatorFork/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_mergedPattern_23Apr2015.root'
+    # 'file:/home/llr/cms/cadamuro/TauFixEmulatorFork2/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_34.root'
+    # 'file:/home/llr/cms/cadamuro/TauFixEmulatorFork/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_TEST.root'
+    # 'file:/home/llr/cms/cadamuro/TauFixEmulatorFork/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_run271306Pier.root'
+    # 'file:/home/llr/cms/cadamuro/TauFixEmulatorFork2/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_SudanCapture_data_calol2_160422_13_demux_15.root'
+    # 'file:/home/llr/cms/cadamuro/TauFixEmulatorFork2/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_data_calol2_160422_13_demux_15_isoFix.root'
+    #'file:/home/llr/cms/cadamuro/TauFixEmulatorFork2/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_34.root',
+    #'file:/home/llr/cms/cadamuro/TauFixEmulatorFork2/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_35.root',
+    #'file:/home/llr/cms/cadamuro/TauFixEmulatorFork2/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_38.root',
+    # 'file:/home/llr/cms/cadamuro/TauFixEmulatorFork2/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_Sudan30Apr.root'
+    # 'file:/home/llr/cms/cadamuro/TauFixEmulatorFork2/CMSSW_8_0_2/src/RunEmulator/l1tCalo_2016_simEDM_Sudan2Mag.root'
+    'file:/home/llr/cms/strebler/L1_eg/fw_tests/P5_capture_files/data_calol2_160429_16_trunkdemux/good/demux/l1tCalo_2016_simEDM.root' # NB: demux only!!
+    )
 )
+# process.source = cms.Source("PoolSource",
+#     fileNames = FILELIST
+# )
 
 process.load("Configuration.StandardSequences.GeometryDB_cff")
 process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
 process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10000)
+    # input = cms.untracked.int32(1000)
+    input = cms.untracked.int32(-1)
 )
 
 ## init plugin
@@ -51,7 +67,12 @@ process.TreeEmul = process.TreeData.clone(
 )
 
 # process.TFileService=cms.Service('TFileService',fileName=cms.string('L1Ntuple_allEvts_MinBias_266667_lastEmulTag.root'))
-process.TFileService=cms.Service('TFileService',fileName=cms.string('L1Ntuple_allEvts_MinBias_266667_testDemux.root'))
+# process.TFileService=cms.Service('TFileService',fileName=cms.string('/home/llr/cms/cadamuro/MWGR_802/CMSSW_8_0_2/src/RunOnPattern/dumpedFromData/L1Ntuple_allEvts_AndyEmul_184Evts.root'))
+# process.TFileService=cms.Service('TFileService',fileName=cms.string('L1Ntuple_reEmulSecTrimFix.root'))
+# process.TFileService=cms.Service('TFileService',fileName=cms.string('L1Ntuple_reEmulWithGoodCalib_hasEMFix_34.root'))
+#process.TFileService=cms.Service('TFileService',fileName=cms.string('L1Ntuple_finalTestAllFixes_1000Taus.root'))
+# process.TFileService=cms.Service('TFileService',fileName=cms.string('L1Ntuple_Sudan30Apr.root'))
+process.TFileService=cms.Service('TFileService',fileName=cms.string('L1Ntuple_Sudan2MagDemux.root'))
 
 # if isEmulated:
 #     process.TFileService=cms.Service('TFileService',fileName=cms.string('L1Ntuple_emul.root'))
